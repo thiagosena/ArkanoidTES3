b798ac15197ebc7109c99cb53e600e19f4f62e4b
Thiago César <lord.sena@gmail.com>
2013-11-27 16:38:04 -0300

    testes na aplicação real

--- test/test-calc.js
+++ test/test-calc.js
@@ -1,11 +1,13 @@
 var assert = require('assert'),
     vows = require('vows'),
 	 arkanoidTest = require('/home/travis/build/thiagosena/ArkanoidTES3/js/arkanoid');
-   
+
+var gravity = new arkanoidTest.b2Vec2(0, 0);
+var world = new arkanoidTest.b2World(gravity, false);
 
 vows.describe('audio').addBatch({
   'When performing playDestroySound': {
-    topic: arkanoidTest.init(),
+    topic: arkanoidTest.addBlock(world, 20, 350),
     'result should be valid': function (result) {
       assert.equal(result, true);
     }
